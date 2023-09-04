39030fe74e92a3ab62aa4f0644c95d2004a7cbb4
Leandro Lillo <llillo@medallia.com>
2023-09-04 10:56:11 -0300

    Added basic command line parsing

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -14,39 +14,106 @@
 
 #include "demo/PlaygroundTest.h"
 
-int main(int argn, char** argv){
+/**
+ * From https://stackoverflow.com/questions/865668/parsing-command-line-arguments-in-c
+ */
+class InputParser{
+    public:
+        InputParser (int &argc, char **argv){
+            for (int i=1; i < argc; ++i)
+                this->tokens.push_back(String(argv[i]));
+        }
+        /// @author iain
+        const String& getCmdOption(const String &option) const{
+            std::vector<String>::const_iterator itr;
+            itr =  std::find(this->tokens.begin(), this->tokens.end(), option);
+            if (itr != this->tokens.end() && ++itr != this->tokens.end()){
+                return *itr;
+            }
+            static const String empty_string("");
+            return empty_string;
+        }
+
+        const String &getArg(unsigned char position) {
+//        	for(int index = 0; index < tokens.size(); index++) {
+//        		printf("Token[%d]: [%s]\n", index, tokens[index].c_str());
+//        	}
+
+        	if(position < tokens.size()) {
+        		return this->tokens[position];
+        	}
+
+        	static const String empty_string("");
+            return empty_string;
+        }
+
+        /// @author iain
+        bool cmdOptionExists(const String &option) const{
+            return std::find(this->tokens.begin(), this->tokens.end(), option)
+                   != this->tokens.end();
+        }
+    private:
+
+
+        std::vector <String> tokens;
+
+};
+
+
+/**
+ * Usage: playground [playground name]
+ *	where [playground name] in [GeneralDemo, ObjDemo, CollisionDetection, Terrain, Physics, RoadFighter]
+ * Note that tests are always ran
+ */
+int main(int argc, char** argv){
 
 	String repository = Paths::getDirname(argv[0]) + "/../../media"; //assumes executable lies in playground/target folder
 
-    TestsPlayground *playgroundtests = new TestsPlayground(repository);
-    playgroundtests->withName("Playground Tests")->run();
-    delete playgroundtests;
 
-//    PlaygroundGeneralDemo *playground = new PlaygroundGeneralDemo(repository);
-//    playground->run();
-//    delete playground;
-//
-//    ObjDemoPlayground *objPlayground = new ObjDemoPlayground(repository);
-//    objPlayground->run();
-//    delete objPlayground;
-//
-
-//    CollisionDetectionPlayground *colDetplayground = new CollisionDetectionPlayground(repository);
-//    colDetplayground->withName("Collision Detection Playground")->run();
-//    delete colDetplayground;
-//
-//    PlaygroundTerrainDemo * terrainPlayground = new PlaygroundTerrainDemo(repository);
-//    terrainPlayground->withName("Terrain Demo")->run();
-//    delete terrainPlayground;
+	//TODO: Move this to a list of playgrounds and pick the one to run based on its name. Also list available names from this list.
+	InputParser input(argc, argv);
+	if(input.cmdOptionExists("-h") || input.cmdOptionExists("--help")){
+		printf("Usage: %s [playground name]\n", Paths::getBasename(argv[0]).c_str());
+		printf("	where [playground name] in [GeneralDemo, ObjDemo, CollisionDetection, Terrain, Physics, RoadFighter]\n");
+		printf("Note that tests are always ran\n");
+	} else
+	{
+		String playgroundName = StringUtils::toLowercase(StringUtils::trim(input.getArg(0)));
+		std::remove_if(playgroundName.begin(), playgroundName.end(), [](unsigned char ch) { return !std::isspace(ch); });
 
 
-//    PhysicsPlayground *physicsPlayground = new PhysicsPlayground(repository);
-//    physicsPlayground->withName("Physics")->run();
-//    delete physicsPlayground;
+		printf("Running playground [%s]\n", playgroundName.c_str());
 
-//    RoadFighter *roadFighter = new RoadFighter(repository);
-//    roadFighter->withName("Road Fighter")->run();
-//    delete roadFighter;
+	    TestsPlayground *playgroundtests = new TestsPlayground(repository);
+	    playgroundtests->withName("Playground Tests")->run();
+	    delete playgroundtests;
+
+	    if(playgroundName == "generaldemo") {
+			Playground *playground = new PlaygroundGeneralDemo(repository);
+			playground->withName("GeneralDemo")->run();
+			delete playground;
+	    } else if(playgroundName == "ObjDemo") {
+	    	Playground *playground = new ObjDemoPlayground(repository);
+			playground->withName("ObjDemo")->run();
+			delete playground;
+	    } else if(playgroundName == "collisiondetection") {
+	    	Playground *playground = new CollisionDetectionPlayground(repository);
+			playground->withName("Collision Detection Playground")->run();
+			delete playground;
+	    } else if(playgroundName == "terrain") {
+	    	Playground *playground = new PlaygroundTerrainDemo(repository);
+			playground->withName("Terrain Demo")->run();
+			delete playground;
+	    } else if(playgroundName == "physics") {
+	    	Playground *playground = new PhysicsPlayground(repository);
+			playground->withName("Physics")->run();
+			delete playground;
+	    } else if(playgroundName == "roadFighter") {
+	    	Playground *playground = new RoadFighter(repository);
+			playground->withName("Road Fighter")->run();
+			delete playground;
+	    }
+	}
 
     printf("done\n");
     return 0;
